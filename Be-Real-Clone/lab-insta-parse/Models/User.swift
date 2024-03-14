d59424d65e89de02cfae3a8abfcca34042d93cba
William Sosropartono <wcsosropartono@gmail.com>
2024-03-13 22:47:09 -0700

    Uploaded proj

---
+++ Be-Real-Clone/lab-insta-parse/Models/User.swift
@@ -0,0 +1,31 @@
+//
+//  User.swift
+//  lab-insta-parse
+//
+//  Created by Charlie Hieger on 11/28/22.
+//
+
+import Foundation
+import ParseSwift
+
+struct User: ParseUser {
+    
+    var objectId: String?
+    var createdAt: Date?
+    var updatedAt: Date?
+    var ACL: ParseACL?
+    var originalData: Data?
+    
+    var username: String?
+    var email: String?
+    var emailVerified: Bool?
+    var password: String?
+    var authData: [String : [String : String]?]?
+    
+}
+
+// TODO: Pt 1 - Import Parse Swift
+
+
+// TODO: Pt 1 - Create Parse User model
+// https://github.com/parse-community/Parse-Swift/blob/3d4bb13acd7496a49b259e541928ad493219d363/ParseSwift.playground/Pages/3%20-%20User%20-%20Sign%20Up.xcplaygroundpage/Contents.swift#L16
